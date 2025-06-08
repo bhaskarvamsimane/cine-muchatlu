298c1b7703e1a627050c044310cbc2856c05cb7e
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-08 12:51:46 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,4 +5,4 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
