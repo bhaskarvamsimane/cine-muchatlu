551f09cc6ed4ffe9b13a7754319789f7ac1f752b
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-08 18:33:33 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,6 +11,5 @@ class Post(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
