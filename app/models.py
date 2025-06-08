ffa629e8a7a2101eca044d787d2c6b55460cdf16
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-08 21:17:42 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,15 @@
 from datetime import datetime
 from app import db
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
