dae60fe2332ca261acbe39093b1853b94e8b8b7f
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-08 16:48:46 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,4 +5,5 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(100), nullable=True, default='default.jpg')  # Add this line
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
