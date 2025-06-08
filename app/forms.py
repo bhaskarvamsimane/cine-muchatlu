361d2805e6a92f12257882ce21a275d09f935d5c
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-08 18:57:01 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -15,7 +15,9 @@ from wtforms.validators import DataRequired, EqualTo
 
 class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password', message='Passwords must match.')
+    ])
     submit = SubmitField('Sign Up')
