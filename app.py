a8fa37b30c70ada30cd0b54ecf29ae4ed9fe4ae6
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-08 16:54:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ app = Flask(__name__)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    message = None
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
         
-        # Dummy success message (you can later connect to DB)
-        return render_template('signup.html', message="User registered successfully!")
-
-    return render_template('signup.html', message=None)
+        # Here you can add code to save user to DB
+        message = "User registered successfully!"
+        
+    return render_template('signup.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
