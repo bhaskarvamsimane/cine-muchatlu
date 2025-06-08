b1262f799ef5631cdc6563e06325e57ecc8347a4
bhaskarvamsimane <155291967+bhaskarvamsimane@users.noreply.github.com>
2025-06-08 20:49:52 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -11,6 +11,7 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
 from wtforms.validators import DataRequired, EqualTo
 
 class SignUpForm(FlaskForm):
